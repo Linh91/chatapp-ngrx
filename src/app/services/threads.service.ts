ccaffb55c581f360efb1bb1b9e31bc94ca3b8f42
sammy.ha <sammy.ha@kingfisher.com>
2018-04-19 11:27:44 +0100

    calling the backend in threads service

--- src/app/services/threads.service.ts
+++ src/app/services/threads.service.ts
@@ -1,13 +1,16 @@
+import { AllUserData } from './../../../shared/to/all-user-data';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/';
+import { Http } from '@angular/http';
+
 
 @Injectable()
 export class ThreadsService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
-  loadUserThreads(): Observable<> {
-
+  loadUserThreads(): Observable<AllUserData> {
+    return this.http.get('/api/threads')
+    .map(res => res.json());
   }
-
 }
